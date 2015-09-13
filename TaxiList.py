359d0b1106aaec8a0d2e8dc878ad1208c37fb52e
Lingyuan Ji <LingyuanJi@Proton.local>
2015-09-13 16:44:31 +0800

    manual merge

--- TaxiList.py
+++ TaxiList.py
@@ -9,17 +9,9 @@ class TaxiList(object):
         self.Tlist = []
 
     def generate(self,taxi_num,citymap):
-<<<<<<< HEAD
-        rd_id = np.random.randint(point_num, size=length)
-        rd_id_tuple_list = [(rd_id[i], citymap.random_choose(rd_id[i])) for i in range(length)]
-        rd_location = np.random.random(length)#model undecided
-        self.Tlist = [Taxi(rd_id_tuple_list[i], rd_location[i]) for i in range(taxi_num)]
-
-=======
         rd_id = np.random.randint(len(citymap.coordinate), size=taxi_num)
         rd_id_tuple_list = [(rd_id[i], citymap.random_choose(rd_id[i])) for i in range(taxi_num)]
         rd_location = np.random.random(taxi_num)#model undecided
-        
+
         self.Tlist = [Taxi(Position(rd_id_tuple_list[i], rd_location[i]),Constants["velocity"]) for i in range(taxi_num)]
-        
->>>>>>> d0b6278c127cb198c569dcc696f23952ad0510aa
+
